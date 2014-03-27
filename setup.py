a938721530c7f5949408fc855ae0690a0f481c0f
rory.mccann <rory.mccann@cab092d8-7db4-334c-8fbf-65bf24401775>
2014-03-27 13:11:15 +0000

    Rename some directories to make them installable when using pip & subdirectories
    If the egg is named differently from the subdirectory, pip won't be able
    to install it.
    
    git-svn-id: svn://172.31.1.3/Development/Common/ThirdParty/Python@13468 cab092d8-7db4-334c-8fbf-65bf24401775

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name = "oauth2app",
+
+    version = "0.3.0.1",
+
+    packages = find_packages(),
+
+    install_requires = ['Django>=1.2.3', "django-uni-form>=0.8.0"],
+    include_package_data = True,
+
+    # metadata for upload to PyPI
+    author = "John Wehr",
+    author_email = "johnwehr@gmail.com",
+    description = "Django OAuth 2.0 Server App",
+    license = "MIT License",
+    keywords = "django oauth2 oauth app server",
+    url = "https://github.com/hiidef/oauth2app"
+
+)
---
+++ tests/testsite/apps/api/tests/__init__.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+from .scope import ScopeTestCase
+from .test_json import JSONTestCase
+from .mac import MACTestCase
+from .bearer import BearerTestCase
+from .responsetype import ResponseTypeTestCase
+from .config import ConfigTestCase
